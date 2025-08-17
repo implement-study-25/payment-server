7420e116e3371c43dd13fcd62b37ad851f77c7c6
pyjhoop <pyjhoop@naver.com>
2025-08-17 09:36:46 +0900

    docs: 결제승인 테이블 init.sql 생성
    #4

---
+++ src/main/resources/static/database/init.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS payment;
+
+CREATE TABLE payment (
+                         payment_id bigint NOT NULL AUTO_INCREMENT COMMENT '결제 고유 ID',
+                         order_no varchar(100) NOT NULL COMMENT '주문번호' UNIQUE,
+                         mall_id varchar(100) NOT NULL COMMENT '상점 ID',
+                         transaction_id varchar(100) NOT NULL COMMENT 'transaction id' UNIQUE,
+                         amount int DEFAULT NULL COMMENT '결제 승인 가격',
+                         currency enum('AUD','BRL','CAD','CHF','CNY','DKK','EUR','GBP','HKD','INR','JPY','KRW','MXN','NOK','RUB','SEK','SGD','THB','USD','VND') COMMENT '통화',
+                         status enum('CANCELED','CONFIRM','FAILED') COMMENT '승인 상태',
+                         created_at datetime(6) DEFAULT NULL COMMENT '생성일',
+                         updated_at datetime(6) DEFAULT NULL COMMENT '수정일',
+                         PRIMARY KEY (`payment_id`)
+) COMMENT = '결제 승인 정보 테이블';
